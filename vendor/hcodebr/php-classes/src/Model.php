4b0e5600e471c9fa6cc6b794f316c0df0a5e6d29
Mateus Leandro <mateusleandroml@hotmail.com>
2019-01-10 15:53:43 -0200

    aula 115

--- vendor/hcodebr/php-classes/src/Model.php
+++ vendor/hcodebr/php-classes/src/Model.php
@@ -24,7 +24,7 @@ class Model{
 
 	public function setData($data = array()){
 
-		foreach ($data as $key => $value) {
+		foreach ($data as $key => $value) {			
 			$this->{"set".$key}($value);	
 		}
 	}
