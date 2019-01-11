c86dbda3aeabf0c2ce7fb2854e03f51e94f423ce
Mateus Leandro <mateusleandroml@hotmail.com>
2019-01-11 07:50:16 -0200

    Carrinho Produtos

--- vendor/hcodebr/php-classes/src/Model/Cart.php
+++ vendor/hcodebr/php-classes/src/Model/Cart.php
@@ -75,8 +75,7 @@ class Cart extends Model{
 	}
 
 	public function addProduct(Product $product){
-		$sql = new Sql();
-		var_dump($this->getidcart(),$product->getidproduct());exit;
+		$sql = new Sql();		
 		$sql->query("INSERT INTO tb_cartsproducts (idcart, idproduct) VALUES(:idcart, :idproduct)",[
 			":idcart"=>$this->getidcart(),
 			":idproduct"=>$product->getidproduct()
@@ -84,16 +83,17 @@ class Cart extends Model{
 	}
 
 	public function removeProduct(Product $product, $all = false){
-		
 		$sql = new Sql();
 		if($all){			
+
 			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL",[
-				":idcart"=>$this->idcart(),
+				":idcart"=>$this->getidcart(),
 				":idproduct"=>$product->getidproduct()
 			]);
 		}else{			
+			
 			$sql->query("UPDATE tb_cartsproducts SET dtremoved = NOW() WHERE idcart = :idcart AND idproduct = :idproduct AND dtremoved IS NULL LIMIT 1",[
-				":idcart"=>$this->idcart(),
+				":idcart"=>$this->getidcart(),
 				":idproduct"=>$product->getidproduct()
 			]);
 		}
