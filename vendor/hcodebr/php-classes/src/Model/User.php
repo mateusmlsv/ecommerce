9f8b8b8773b1cff2a80ef1151db16caef7965c0d
Mateus Leandro <mateusleandroml@hotmail.com>
2019-02-14 09:37:50 -0200

    Minha Conta

--- vendor/hcodebr/php-classes/src/Model/User.php
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -13,6 +13,7 @@ class User extends Model{
 	const SECRET = "HcodePhp7_Secret";
 	const ERROR = "UserError";
 	const ERROR_REGISTER = "UserErrorRegister";
+	const SUCCESS = "UserSuccess";
 
 	public static function getFromSession(){
 
@@ -247,7 +248,7 @@ class User extends Model{
 	}
 
 	public static function getError(){
-		$msg = (isset($_SESSION[User::ERROR])) ? $_SESSION[User::ERROR] : "";
+		$msg = (isset($_SESSION[User::ERROR]) && $_SESSION[User::ERROR]) ? $_SESSION[User::ERROR] : "";
 		User::clearError();
 		return $msg;
 	}
@@ -256,6 +257,24 @@ class User extends Model{
 		$_SESSION[User::ERROR] = NULL;
 	}
 
+	/**********************************************/
+
+	public static function setSuccess($msg){
+		$_SESSION[User::SUCCESS] = $msg;
+
+	}
+
+	public static function getSuccess(){
+		$msg = (isset($_SESSION[User::SUCCESS]) && $_SESSION[User::SUCCESS]) ? $_SESSION[User::SUCCESS] : "";
+		User::clearSuccess();
+		return $msg;
+	}
+
+	public static function clearSuccess(){
+		$_SESSION[User::SUCCESS] = NULL;
+	}
+
+	/************************************************/
 	public static function setErrorRegister($msg){
 		$_SESSION[User::ERROR_REGISTER] = $msg;
 	}
