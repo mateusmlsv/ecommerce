c167fa4eb32663b42fedc2e66f1540bb3f1a4f46
Mateus Leandro <mateusleandroml@hotmail.com>
2019-02-15 10:34:32 -0200

    Meus Pedidos e Resumo Carinho

--- functions.php
+++ functions.php
@@ -1,6 +1,7 @@
 <?php 
 
 use \Hcode\Model\User;
+use \Hcode\Model\Cart;
 
 function formatPrice($vlprice){
 	if(!$vlprice > 0) $vlprice = 0;
@@ -15,5 +16,16 @@ function getUserName(){
 	$user = User::getFromSession();		
 	return $user->getdesperson();
 }
+function getCartNrQtd(){
+	$cart = Cart::getFromSession();
+	$totals = $cart->getProductsTotals();
+	return $totals['nrqtd'];
+}
+function getCartVlSubTotal(){
+	$cart = Cart::getFromSession();
+	$totals = $cart->getProductsTotals();
+	return formatPrice($totals['vlprice']);
+}
+
 
  ?>
